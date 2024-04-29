6bb0422b001ef1627fd19b8c50332c9dce49c859
RuyaTechOrg <aruyachristine65@gmail.com>
2024-04-29 16:50:30 +0100

    new changes

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -8,6 +8,9 @@ import TestimonialImage06 from '@/public/images/streemhub3.png';
 import TestimonialImage03 from '@/public/images/Screenshot Capture - 2024-04-24 - 23-13-27.png';
 import TestimonialImage04 from '@/public/images/Screenshot Capture - 2024-04-25 - 13-51-08.png';
 import TestimonialImage05 from '@/public/images/Screenshot Capture - 2024-04-25 - 13-54-15.png';
+import TestimonialImage08 from '@/public/images/iPhone 14 & 15 Pro Max - 1.png';
+import TestimonialImage07 from '@/public/images/iPhone 14 & 15 Pro - 1.png';
+import TestimonialImage09 from '@/public/images/iPhone 14 & 15 Pro Max - 1 (1).png';
 export const metadata =  {
     title : 'Products  - Ruyatech',
     description : 'A showcase of products {software}.',
@@ -146,6 +149,7 @@ export default function DevPage () {
             {/* 1st testimonial */}
             <div className="flex flex-col h-full p-6 bg-gray-800" data-aos="fade-up">
             <div className="text-gray-700 font-medium mt-6 pt-5 border-t border-gray-700">
+            <Image src={TestimonialImage07} alt="product img" className="pb-5"/>
                 <cite className="text-gray-200 not-italic">CryptoWall - RuyaTech,</cite> - <a className="text-purple-600 hover:text-gray-200 transition duration-150 ease-in-out" href="#0">Coming soon...</a>
               </div>
             </div>
@@ -155,12 +159,14 @@ export default function DevPage () {
               <div>
               </div>
               <div className="text-gray-700 font-medium mt-6 pt-5 border-t border-gray-700">
+              <Image src={TestimonialImage08} alt="product img"/>
               </div>
             </div>
 
             {/* 3rd testimonial */}
             <div className="flex flex-col h-full p-6 bg-gray-800" data-aos="fade-up" data-aos-delay="400">
             <div className="text-gray-700 font-medium mt-6 pt-5 border-t border-gray-700">
+            <Image src={TestimonialImage09} alt="product img"/>
               </div>
             </div>
 
