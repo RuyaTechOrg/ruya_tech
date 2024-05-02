f0a2920004663ef1894f7a379bbbac0a1fda93fd
RuyaTechOrg <aruyachristine65@gmail.com>
2024-05-02 19:11:07 +0100

    new changes

--- app/About/page.tsx
+++ app/About/page.tsx
@@ -29,65 +29,50 @@ export default function AboutUs() {
           </div>
 
           <div className="max-w-6xl mx-start pb-12 md:pb-20">
-            <h1 className="h1 mb-4">Education</h1>
-            <p className="text-xl text-purple-600">""</p>
+            <h1 className="h3 mb-4">Education</h1>
+            <p className="text-xl text-purple-600">"Educational Progress through the years"</p>
           </div>
           {/* <div className="max-w-6xl mx-start pb-12 md:pb-20">
             <Image src={aboutImgSrc} alt={"image here"} width={1100} height={1000}/>
           </div> */}
           <div className="max-w-6xl mx-start pb-12 md:pb-20">
-            <p className="text-xl text-white">“Take these,” said the old man, holding out a white stone and a 
-            black stone that had been embedded at the center of the 
-            breastplate. “They are called Urim and Thummim. The black 
-            signifies ‘yes,’ and the white ‘no.’ When you are unable to read the 
-            omens, they will help you to do so. Always ask an objective 
-            question.
-            “But, if you can, try to make your own decisions. The treasure is 
-            at the Pyramids; that you already knew. But I had to insist on the 
-            payment of six sheep because I helped you to make your decision.”
-            The boy put the stones in his pouch. From then on, he would 
-            make his own decision</p>
+            <p className="text-xl text-white">
+              I attended primary school at Apex Nursery and primary School,Nigeria. from <strong className="text-purple-500">2004 - 2010</strong>.
+              Soon after i began secondary school at Federal Government College Malali Kaduna. I went through Jss1 - Jss3. My family and I  had to relocate. I then at to finish  the remaining three years of my secondary school Education
+              at Federal Government College Ibillo, Edo state Nigeria<strong className="text-purple-500"> (2018)</strong>.
+              I am currently studying for bachelors degree for<strong className="text-purple-500"> Mechatronics Engineering</strong> at University of Benin, Edo State Nigeria. <strong>Currently in my fourth year</strong>
+            </p>
           </div>
 
           <div className="max-w-6xl mx-start pb-12 md:pb-20">
-            <h1 className="h1 mb-4">Tech - Journey</h1>
-            <p className="text-xl text-purple-600">"Building and pioneering decentralized systems"</p>
+            <h1 className="h3 mb-4">Tech - Journey</h1>
+            <p className="text-xl text-purple-600">"For as long as i could remember, I have always to loved to code"</p>
           </div>
           {/* <div className="max-w-6xl mx-start pb-12 md:pb-20">
             <Image src={aboutImgSrc} alt={"image here"} width={1100} height={1000}/>
           </div> */}
           <div className="max-w-6xl mx-start pb-12 md:pb-20">
-            <p className="text-xl text-white">“Take these,” said the old man, holding out a white stone and a 
-            black stone that had been embedded at the center of the 
-            breastplate. “They are called Urim and Thummim. The black 
-            signifies ‘yes,’ and the white ‘no.’ When you are unable to read the 
-            omens, they will help you to do so. Always ask an objective 
-            question.
-            “But, if you can, try to make your own decisions. The treasure is 
-            at the Pyramids; that you already knew. But I had to insist on the 
-            payment of six sheep because I helped you to make your decision.”
-            The boy put the stones in his pouch. From then on, he would 
-            make his own decision</p>
+            <p className="text-xl text-white">I've always wanted to code for as long as i realised programming was a thing.
+            But i started coding for real just before secondary school ended (2018). I started learing HTMl, CSS , Javascript and then react. Built a couple of projects and soon after i moved into mobile app developement. I have always liked to challenge myself.... 
+            I started learning mobile app development using reactnative, soon after i interned for a company - <strong className="text-purple-500">HavocWeb Tech(2022 -2023)</strong>.
+            I learnt alot about mobile app development whilst working on a couple of projects.<strong className="text-purple-500">(MobylWallet)</strong>.
+            I then worked on a couple of projects with other startups with the sole-intent of gaining experience. I once worked on a mobile application <strong className="text-purple-500">Paywave</strong> using flutter. Whilst i found the technology a bit strange and new, i soon got the hang of it and before i long i was very good with flutter.
+            Overall, Although the journey wasn't easy i have gained alot of programming experience over the years.
+            Now i mostly worked on projects to challenge myself and improve my coding skills.
+            </p>
           </div>
           <div className="max-w-6xl mx-start pb-12 md:pb-20">
             <h1 className="h1 mb-4">Future Plans</h1>
-            <p className="text-xl text-purple-600">"Building Quality Software to solve problems and handle tasks"</p>
+            <p className="text-xl text-purple-600">"What plans lied ahead of me"</p>
           </div>
           {/* <div className="max-w-6xl mx-start pb-12 md:pb-20">
             <Image src={aboutImgSrc} alt={"image here"} width={1100} height={1000}/>
           </div> */}
           <div className="max-w-6xl mx-start pb-12 md:pb-20">
-            <p className="text-xl text-white">“Take these,” said the old man, holding out a white stone and a 
-            black stone that had been embedded at the center of the 
-            breastplate. “They are called Urim and Thummim. The black 
-            signifies ‘yes,’ and the white ‘no.’ When you are unable to read the 
-            omens, they will help you to do so. Always ask an objective 
-            question.
-            “But, if you can, try to make your own decisions. The treasure is 
-            at the Pyramids; that you already knew. But I had to insist on the 
-            payment of six sheep because I helped you to make your decision.”
-            The boy put the stones in his pouch. From then on, he would 
-            make his own decision</p>
+            <p className="text-xl text-white">
+              In the next 5  years or there about i hope to find myself having alot fun, both as programmer and life in general.
+              I hope to have challenge myself and escaped my comfort zones. I hope to have built whatever i want to using my skills as a developer and programmer.
+            </p>
           </div>
         </div>
       </div>
